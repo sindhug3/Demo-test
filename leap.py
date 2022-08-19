1895f70638edf26d97e5fa02cf1f28100e5243b1
sindhug3 <111479767+sindhug3@users.noreply.github.com>
2022-08-19 18:57:27 +0530

    Update leap.py

--- leap.py
+++ leap.py
@@ -2,17 +2,17 @@ class leap(object):
     def year(self,a):
         # year= int(input("Enter the year-"))
         if (a % 400 == 0) and (a % 100 == 0):
-            # print(f"{a} ia a leap year")
-            return a
+            print(f"{a} ia a leap year")
+#             return a
         elif (a % 4 == 0) and (a % 100 != 0):
-            # print(f"{a} is a leap year")
-            return a
+            print(f"{a} is a leap year")
+#             return a
         else:
-            #  print(f"{a} is not a leap year") 
-            return a  
+            print(f"{a} is not a leap year") 
+#             return a  
     
-# dog=year(2020)
-# print(dog)
+dog=year(2020)
+print(dog)
 
 
 

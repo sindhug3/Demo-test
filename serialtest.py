59b7f65a6c6a12ebbc5693e33427f5b34e9a1a3f
sindhug3 <111479767+sindhug3@users.noreply.github.com>
2022-08-22 13:35:52 +0530

    Add files via upload

---
+++ serialtest.py
@@ -0,0 +1,16 @@
+import serial
+import time
+class serialtest(object):
+    def test(self,a):
+        ser = serial.Serial ("COM5")    #Open named port 
+        ser.baudrate = 9600           #Set baud rate to 9600
+        #Read ten characters from serial port to data
+        ser.write(b'hello world') 
+        time.sleep(1)
+        data1 = ser.read(8)
+        return data1
+        #Send back the received data
+        ser.close() 
+    
+# a=serialtest()
+# print(a.test(a))
